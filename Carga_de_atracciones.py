50f9a3c41f05e63ad36fff870784ea1360824193
NicolasKowal <nicolas.kowal95@gmail.com>
2023-05-22 15:19:38 -0300

    archivo para agregar atracciones

--- Carga_de_atracciones.py
+++ Carga_de_atracciones.py
@@ -1,20 +1,31 @@
 from funciones import *
 
 
-print("ESTRUCTURA: NOMBRE, TIPO DE ATRACCION, TIEMPO, DINERO")
-usuarios = abir_archivo("perfiles.json")
+print("ESTRUCTURA:  NOMBRE, PRECIO, TIEMPO, CUPO, TIPO")
+atracciones = abir_archivo("atracciones.json")
 continuar = 1
 while continuar == 1:
-    usuario = []
+    atraccion = []
 
     nombre = input("Nombre: ")
     nombre = nombre.capitalize()
-    for user in usuarios:
+    for user in atracciones:
         if nombre == user[0]:
             print("NOMBRE REPETIDO.")
             break
     else:
-        usuario.append(nombre)
+        atraccion.append(nombre)
+        
+        precio = limites(0, 10000, "Precio: ")
+        atraccion.append(precio)
+        
+        tiempo = limites(0, 10000, "Tiempo: ")
+        atraccion.append(tiempo)
+
+        cupo = limites(0, 1000, "Cupo: ")
+        atraccion.append(cupo)
+        
+        
         tipodeatraccion = limites(
             1, 3, "1. Aventura\n2. Degustacion\n3. Paisajes\n")
         if tipodeatraccion == 1:
@@ -23,11 +34,8 @@ while continuar == 1:
             tipodeatraccion = "Degustacion"
         elif tipodeatraccion == 3:
             tipodeatraccion = "Paisajes"
-        usuario.append(tipodeatraccion)
-        tiempo = limites(0, 10000, "Tiempo disponible: ")
-        usuario.append(tiempo)
-        dinero = limites(0, 10000, "Dinero disponible: ")
-        usuario.append(dinero)
+        atraccion.append(tipodeatraccion)
+
         continuar = limites(1, 2, "Continuar?\n1. Si\n2. No\n")
-        usuarios.append(usuario)
-guardar_archivo("perfiles.json", usuarios)
+        atracciones.append(atraccion)
+guardar_archivo("atracciones.json", atracciones)
